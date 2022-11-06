cd744cdff96e90c75e6e18dd21aeaffc5509a77f
Olivia <oliviatmanalastas@gmail.com>
2022-11-06 17:29:40 -0500

    home style file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1> Home </h1>
+      <h1 style={{ fontFamily:'Ms Madi'}} className='home-name'> Olivia Manalastas </h1>
     </div>
   )
 }
