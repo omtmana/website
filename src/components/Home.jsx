34096a9f77cc77976ead0e9812a6897d8f0cdc0b
Olivia <oliviatmanalastas@gmail.com>
2022-11-07 11:13:02 -0500

    Fixed home header banner

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,11 +4,14 @@ import '../styles/Home.css'
 const Home = () => {
   return (
     <div className='home'>
-      <h1 className='home-name'> Olivia Manalastas </h1>
+      <div className='home-header-banner'>
+        <h1 className='home-name'> Olivia Manalastas </h1>
+        <h2 style={{fontFamily:'Ms Madi', fontSize:'3em'}}> Software Engineer </h2> 
+      </div>
       <div className='home-info-buttons'>
-        <button> LinkedIn </button>
-        <button> Github </button>
-        <button> Resume </button>
+        <button className='home-button'> LinkedIn </button>
+        <button className='home-button'> Github </button>
+        <button className='home-button'> Resume </button>
       </div>
     </div>
   )
