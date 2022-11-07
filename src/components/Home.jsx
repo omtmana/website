7cfa483ff23104bf3257a08eb21328fbe2a277c9
Olivia <oliviatmanalastas@gmail.com>
2022-11-07 11:30:49 -0500

    Added home style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,14 +5,17 @@ const Home = () => {
   return (
     <div className='home'>
       <div className='home-header-banner'>
-        <h1 className='home-name'> Olivia Manalastas </h1>
-        <h2 style={{fontFamily:'Ms Madi', fontSize:'3em'}}> Software Engineer </h2> 
+        <div>
+          <h1 className='home-name'> Olivia Manalastas </h1>
+          <h2 style={{ fontFamily: 'Ms Madi', fontSize: '3em', color: '#FF0066' }}> Software Engineer </h2>
+          <button className='home-button'> LinkedIn </button>
+          <button className='home-button'> Github </button>
+          <button className='home-button'> Resume </button>
+        </div>
+        <img src='profile_pic.png' className='home-image' />
       </div>
-      <div className='home-info-buttons'>
-        <button className='home-button'> LinkedIn </button>
-        <button className='home-button'> Github </button>
-        <button className='home-button'> Resume </button>
-      </div>
+      {/* <div className='home-button-container'>
+      </div> */}
     </div>
   )
 }
