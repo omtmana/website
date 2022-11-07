210c3c9286a1c954032c92b8ac5e109ea00ea1d4
Olivia <oliviatmanalastas@gmail.com>
2022-11-07 11:46:51 -0500

    Added pointer cursor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
   const handleContactMe = () => {
     navigate('/contact')
   }
-  
+
   return (
     <div className='home'>
       <div className='home-header-banner'>
@@ -21,10 +21,10 @@ const Home = () => {
           <h1 className='home-name'> Olivia Manalastas </h1>
           <h2 style={{ fontFamily: 'Ms Madi', fontSize: '3em', color: '#FF0066' }}> Software Engineer </h2>
           <h4 className='home-links'>
-            <span onClick={handleLinkedIn}> LinkedIn </span> |
-            <span> Github </span> |
-            <span> Resume </span> |
-            <span onClick={handleContactMe}> Contact Me! </span>
+            <span onClick={handleLinkedIn} className='link-data'> LinkedIn </span> |
+            <span className='link-data'> Github </span> |
+            <span className='link-data'> Resume </span> |
+            <span onClick={handleContactMe} className='link-data'> Contact Me! </span>
           </h4>
           {/* <button className='home-button'> LinkedIn </button>
           <button className='home-button'> Github </button>
