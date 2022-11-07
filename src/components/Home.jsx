5fad2215c06a490001b2d74dfa56403bbf5ef440
Olivia <oliviatmanalastas@gmail.com>
2022-11-07 11:41:45 -0500

    Added home buttons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
 import '../styles/Home.css'
+import {Link} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const navigate = useNavigate()
+
+  const handleLinkedIn = () => {
+    navigate('/about')
+  }
   return (
     <div className='home'>
       <div className='home-header-banner'>
         <div>
           <h1 className='home-name'> Olivia Manalastas </h1>
           <h2 style={{ fontFamily: 'Ms Madi', fontSize: '3em', color: '#FF0066' }}> Software Engineer </h2>
-          <button className='home-button'> LinkedIn </button>
+          <h4 className='home-links'>
+            <span onClick={handleLinkedIn}> LinkedIn </span> |
+            <span> Github </span> |
+            <span> Resume </span> |
+            <span> Contact Me! </span>
+          </h4>
+          {/* <button className='home-button'> LinkedIn </button>
           <button className='home-button'> Github </button>
+          <br></br>
           <button className='home-button'> Resume </button>
+          <button className='home-button'> Contact Me! </button> */}
         </div>
         <img src='profile_pic.png' className='home-image' />
       </div>
