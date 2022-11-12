0115c52a9ac9c1e465bcf3f44c037a61f71c2b6d
Olivia <oliviatmanalastas@gmail.com>
2022-11-12 13:51:40 -0500

    initial commit

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -4,7 +4,12 @@ import '../styles/Code.css'
 const Code = () => {
   return (
     <div className='code'>
-      <h1> Code Page </h1>
+      <h1 style={{fontFamily: 'Ms Madi'}}> Check out my code! </h1>
+      <div>
+        <h1> Scan Me! </h1>
+        <img />
+        <h2> Github </h2>
+      </div>
     </div>
   )
 }
