e4b9bd5e18f849ce607d7e85c6e0758abcaaff73
Olivia <oliviatmanalastas@gmail.com>
2022-12-03 15:42:23 -0500

    changed about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,11 +4,11 @@ import '../styles/About.css'
 export const About = () => {
   return (
     <div className='about'>
-      <h1 style={{fontFamily: 'ms madi', fontSize: '1em'}}> 
-      Hi! My name is Olivia. 
-      </h1>
       <div className='about-body'>
         <span className='about-list'>
+      <h1 style={{fontFamily: 'ms madi', fontSize: '3em'}}> 
+      Hi! My name is Olivia. 
+      </h1>
           <p> 📍 Born in America. Grew up in the Philippines. Currently in New York City</p>
           <p> 🧩 Civil Engineering dropout with experience in Mathematics education, Civil Engineering, and Marketing.</p>
           <p> 🧘🏻‍♀️ Aspiring tennis player. V3 in indoor bouldering. 5.10d in indoor top-rope. Occasional futball player</p>
