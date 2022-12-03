114a7754c46050b1d907b7646041ee2d09a3da0e
Olivia <oliviatmanalastas@gmail.com>
2022-12-02 20:38:02 -0500

    added style to codes

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -4,11 +4,17 @@ import '../styles/Code.css'
 const Code = () => {
   return (
     <div className='code'>
-      <h1 style={{fontFamily: 'Ms Madi'}}> Check out my code! </h1>
-      <div>
-        <h1> Scan Me! </h1>
-        <img />
-        <h2> Github </h2>
+      <h1 style={{ fontFamily: 'Ms Madi' }}> Check out my code! </h1>
+      <div className='code-container'>
+        <div className='code-card'>
+          <h1> Scan Me! </h1>
+          <img />
+          <button className='code-button'>Github</button>
+        </div>
+        <div className='code-card'>
+          <h1> Flypto </h1>
+          <button className='code-button'>Github</button>
+        </div>
       </div>
     </div>
   )
