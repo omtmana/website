d6293e71851b6741003edcb352c7db9843aa4f7f
Olivia <oliviatmanalastas@gmail.com>
2022-12-03 23:35:32 -0500

    thinking of adding a picture to about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,13 +7,17 @@ export const About = () => {
       <div className='about-body'>
         <span className='about-list'>
           <h1 style={{ fontFamily: 'ms madi', fontSize: '3em' }}>
-            Hi! My name is <span style={{ color: 'rgb(95, 158, 160)'}}> Olivia </span>.
+            Hi! My name is <span style={{ color: 'rgb(95, 158, 160)' }}> Olivia </span>
           </h1>
+          {/* <img src='random.png' style={{zIndex: '-1', width: '200px'}}/> */}
           <p> 📍 Born in America. Grew up in the Philippines. Currently in New York City</p>
           <p> 🧩 Civil Engineering background with experience in Mathematics education, Civil Engineering, and Marketing.</p>
           <p> 🧘🏻‍♀️ Aspiring tennis player. V3 in indoor bouldering. 5.10d in indoor top-rope. Occasional futball player</p>
           {/* <p> Currently reading: 21 Lessons for the 21st Century </p> */}
         </span>
+        {/* <div>
+          <img src='random.png' style={{ width: '200px', marginBottom: '-5px' }} />
+        </div> */}
       </div>
     </div>
   )
