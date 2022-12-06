cfe87d594883af0a67079f8e9a34b404761b32f1
Olivia <oliviatmanalastas@gmail.com>
2022-12-06 09:52:51 -0500

    working on footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../styles/Layout.css'
+
+const Footer = () => {
+   <div className="footer">
+      <p className="footer-text"> Created by ReactJS</p>
+   </div>
+}
+
+export default Footer
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,14 @@
 import '../styles/Layout.css'
 import React from 'react'
 import Header from './Header'
+import Footer from './Footer'
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Header />
       <main>{children}</main>
+      <Footer />
     </div>
   )
 }
