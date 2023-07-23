4fad86a6ab76c4539fdcba62796b40c224098062
Olivia <oliviatmanalastas@gmail.com>
2023-07-23 17:01:15 -0400

    worked on productions

--- src/components/Production.jsx
+++ src/components/Production.jsx
@@ -20,11 +20,24 @@ const Production = () => {
 
   return (
     <div className='production'>
-      <h1>
+      {/* <h1>
         <span onClick={handleCode} className='production-code'> Codes </span>|
         <span onClick={handleBlog} className='production-blog'> Blogs </span>|
-        <span onClick={handlePicture} className='production-picture'> Pictures </span></h1>
-    </div>
+        <span onClick={handlePicture} className='production-picture'> Pictures </span>
+        </h1> */}
+      <div>
+        <div onClick={handleCode} className='production-code'>
+          <h1> Code </h1>
+        </div>
+        <div onClick={handleBlog} className='production-blog'>
+          <h1> Blogs </h1>
+        </div>
+        <div onClick={handlePicture} className='production-picture'>
+          <h1> Pictures </h1>
+        </div>
+      </div>
+
+   </div>
   )
 }
 
